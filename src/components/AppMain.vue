2ebd789b9c3420fd88a55206d6074885d7aaf3ff
carmelopalumbo <carmelopalumbo@outlook.it>
2022-11-24 16:19:36 +0100

    add-main-structure

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,9 +1,34 @@
 <script>
+import performance from "./partials/performance.vue";
+import results from "./partials/results.vue";
+import services from "./partials/services.vue";
+import team from "./partials/team.vue";
+import news from "./partials/news.vue";
+import knowfirst from "./partials/knowfirst.vue";
+import form from "./partials/form.vue";
+
 export default {
   name: "AppMain",
+  components: {
+    performance,
+    results,
+    services,
+    team,
+    news,
+    knowfirst,
+    form,
+  },
 };
 </script>
 
-<template></template>
+<template>
+  <performance />
+  <results />
+  <services />
+  <team />
+  <news />
+  <knowfirst />
+  <form />
+</template>
 
 <style></style>
